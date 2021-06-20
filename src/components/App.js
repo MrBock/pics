9fa94b06d22c11ad39fe1cb60fbbcf1e7a99e526
Mr_Bock <26689948+MrBock@users.noreply.github.com>
2021-06-20 13:48:08 +0200

    Refactor the unsplash API request to use async await instead of .then.

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization:
-            'Client-ID iUY96ywyCtPUtVg649JITZ2mJj7Yyc9fmpSX3STcq_A',
-        },
-        // .then chains a callback function to the get request via a promise
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID iUY96ywyCtPUtVg649JITZ2mJj7Yyc9fmpSX3STcq_A',
+      },
+    });
+    console.log(response.data.results);
   }
 
   render() {
